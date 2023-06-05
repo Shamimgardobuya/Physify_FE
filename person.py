709900c1511720bda14188635bec2fdaf863b805
Shamimgardobuya <shamimobuya@gmail.com>
2023-06-05 15:10:29 +0300

    testing person.py file

--- person.py
+++ person.py
@@ -1,39 +1,73 @@
 counter = 1
 class Person:
-    
+    age_of_person = []
     def __init__(self,initialAge):
         self.initialAge = initialAge
-        self.age_of_person = []
-       
-        # Add some more code to run some checks on initialAge
-        #using constraints such as as- 1-4 and then -5 to 30
-        #print "Age is not valid, setting age to 0."
-        if initialAge in range(1,5) or initialAge < 1:
+        
+        if  initialAge <= 1:
+            self.initialAge = 0 
             print("Age is not valid, setting age to 0")  
+            # return initialAge
         # self.age_of_person.append(self.initialAge)
     def amIOld(self):
-                # Do some computations in here and print out the correct statement to the console
-                #old when age is >= 18
                 if self.initialAge >= 18:
-                    print("You are old")
+                    return "You are old"
                 elif self.initialAge < 13 :
-                    print("You are young")
+                    return "You are young"
                 elif self.initialAge  >= 13 and self.initialAge < 18 :
-                    print("You are a teenager")
+                    return "You are a teenager"
     def yearPasses(self):
                 
                 self.age_of_person.append(self.initialAge)
                 count = self.age_of_person.count(self.initialAge)
-                if count > 1 :  #
-                  self.initialAge = self.initialAge + count
-                  print(self.age_of_person, count)
-                  return  self.initialAge
+                print()
+                if count == 1  :  # don't count the ones that count == 2
+                  self.initialAge = self.initialAge + count 
+                  return self.initialAge
                 else:
-                    return self.age_of_person[0]
+                    if count > 1 :
+                        self.initialAge = self.initialAge + 1
+                        return self.initialAge
                 #if same person is called twice, increase the age by 1 year.
                 
 
-                # for i in range(0,90):
+              
+t = int(input())  #6
+for i in range(0, t): #0, 6 
+    age = int(input())       #6  
+    p = Person(age)  
+    p.amIOld()    # for first age user input
+    for j in range(0, 3): #iteration will happen thrice
+       p.yearPasses()
+        # print(j)
+    print(j,p.yearPasses())
+    p.amIOld()
+    print("")
+
+# person = Person(18)  #age changes
+# # print(person.yearPasses())  #17 19
+# print(person.yearPasses())# orginal_age as 17
+# print(person.yearPasses())# original age 18 + 1 
+
+#how to capture previous object passed
+
+
+ #   print(f"List every time after increamenting {self.age_of_person, count}")
+                #   print(f"This is the value of the new age {self.initialAge}")
+                #   new_list = []
+                #   [new_list.append(x) for x in self.age_of_person if x not in new_list]
+                #   print('hello')
+                #   print(new_list)
+                  #loop through the list: if the element appears more than once, then remove the duplicate and return the newlist.
+                #   for i in self.age_of_person:
+                #        if self.age_of_person.count(self.age_of_person[i]) > 1:
+                #   no_duplicate = self.age_of_person.count(self.age_of_person) > 1
+                #   self.age_of_person.remove(no_duplicate)
+                #             print(self.age_of_person)
+                #   if self.initial remove duplicate
+
+
+                  # for i in range(0,90):
                       #want ot store the original age value and always increament by 1
 
                       #replace this as new age
@@ -63,21 +97,3 @@ class Person:
                 # if self.initialAge == increase:
                 #       return increase
                 # Increment the age of the person in here
-
-t = int(input())  #6
-for i in range(0, t): #0, 6 
-    age = int(input())       #6  
-    p = Person(age)  
-    p.amIOld()    # for first age user input
-    for j in range(0, 3): #iteration will happen thrice
-        print(p.yearPasses())
-    print(p.yearPasses())
-    p.amIOld()
-    print("")
-
-# person = Person(18)  #age changes
-# # print(person.yearPasses())  #17 19
-# print(person.yearPasses())# orginal_age as 17
-# print(person.yearPasses())# original age 18 + 1 
-
-#how to capture previous object passed
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
 # from print import printing
 from conditionals import checking_n
+from person import Person
 #Arrange,act, asssert, test if it prints out the input function's details
 class testing_printing(unittest.TestCase):
     # def test_printing_input(self):
@@ -8,9 +9,15 @@ class testing_printing(unittest.TestCase):
     #     self.assertEqual(printing(data),f'''{data}.                              
     #                              Hello, World'''
     #                      )
-    def testing_conditionals(self):
-        data =  int(input().strip())
-        self.assertEqual(checking_n(data),"Weird")
+    # def testing_conditionals(self):
+    #     data =  int(input().strip())
+    #     self.assertEqual(checking_n(data),"Weird")
+    def testing_person_created(self):
+        #create your object
+        object_one = Person(-1)
+        self.assertEqual(object_one.amIOld(),print("Age is not valid, setting age to 0"))
+        self.assertEqual(object_one.yearPasses(),1)
+
 #arrange,act assert, - having all values of n, so check n with function by calling, use assertequal
 if __name__ =='__main__':
     unittest.main()
