d5f90bf2575319e6afc3105522c79429f0fc6740
Shamimgardobuya <shamimobuya@gmail.com>
2023-07-20 23:21:51 +0300

    multidimensional array practising with Matplotlib

---
+++ Mdarrays/arr.py
@@ -0,0 +1,80 @@
+#import data files into numpy arrays 
+#to create an array of month names from months.txt which is available for download at 
+#get data from source usually in form of url
+
+#no one will step on humanity as long as I believe- Alhamdulillah
+#then convert format into array
+
+import os
+import numpy as np
+import earthpy as myearth
+import matplotlib.pyplot as plt
+
+month_names_source = 'https://ndownloader.figshare.com/files/12565616'
+# myearth.data.get_data(url=month_names_source)
+os.chdir(os.path.join(myearth.io.HOME, 'earth-analytics')) #set directories to earth-analytics
+
+file_name = os.path.join('data','earthpy-downloads','avg-monthly-precip.txt')
+
+#import  monthly names to numpy array
+# month_names = np.loadtxt(file_name)
+# print(month_names)
+#receiving ValueError: could not convert string to float: 'Jan' when getting numpy array of string content array
+#working with csv files to get data
+precip_2002_2013_url = 'https://ndownloader.figshare.com/files/12707792'
+myearth.data.get_data(url=precip_2002_2013_url)
+myearth.data.get_data(url=month_names_source)
+
+os.chdir(os.path.join(myearth.io.HOME, 'earth-analytics')) 
+file_name_two = os.path.join('data','earthpy-downloads','monthly-precip-2002-2013.csv')
+file_name_2 = os.path.join('data','earthpy-downloads','months.txt')
+
+
+month_precip = np.loadtxt(file_name_two,delimiter=',')
+month_avg = np.loadtxt(file_name)
+# month_names = np.loadtxt(file_name_2)
+
+# month_names = np.loadtxt(file_name_2)
+
+'''Create a new numpy array for the average monthly precipitation 
+in 2013 by selecting all data values in the last row in 
+precip_2002_2013 (i.e. data for the year 2013).'''
+#acess asubarray of 2013
+year_thirteen = month_precip[0:,-1:]#😄 
+# print(np.average(year_thirteen))
+'''Convert the values in the numpy array 
+from inches to millimeters (1 inch = 25.4 millimeters).'''
+
+#get all values of the 2d array to milimetres,
+#loop through all the values of the array
+#identify current unit - inches
+#have a conversion range - 1 inch = 25.4 
+#looping through the precipitation data
+#convert each value to the precipitated data
+#return new array
+
+def converting_measurments(month_precip):
+    new_measurment = month_precip
+    for i in new_measurment:
+        
+        # for i in new_measurment: will loop twice
+        #     i * 25.4
+        return i*25.4
+print(converting_measurments(month_precip))
+#plot with Matpolib
+#Use the converted numpy array
+#  for 2013 and the numpy array 
+# of month names to create plot of Average
+#  Monthly Precipitation in 2013 for Boulder, CO.
+fig,ax = plt.subplots(figsize = (10,8))
+ax.bar(["Jan", "Feb", "Mar", "Apr", "May", "June", "July", 
+          "Aug", "Sept", "Oct", "Nov", "Dec"]
+,
+       month_avg,
+       color='black'
+       )
+
+plt.show()
+
+
+
--- multid.py
+++ multid.py
@@ -20,6 +20,18 @@ import numpy as np
 arr = np.array([[1,2,3],[124,34,12]])
 second_arr = np.array([[0,3,3],[0,1,1]]) 
 third_arr = np.array([[[1,2,3,4,5],[5,4,5,3,2]]])
+#slice the array 🤠 using indexing 
+# array_name[start_index_row:end_index_row,start_index_column:end_index_column]
+
+sliced_arr = arr[1:2] #slicing is per row, row wise 
+#acessing a range of values, 
+# I want  a sub-array containing 124 and 34 
+sliced_arr_two = arr[1:,0:2]
+print(sliced_arr_two)
+
+# print(arr)
+# print(sliced_arr)
+# print(np.shape(arr)) #output ↪️ (2,3) shape of the multidimensional array -  rows first index, columns second
 # print(arr + third_arr) 
 # for i in arr:
     # for i in arr:
@@ -54,8 +66,10 @@ arr3= np.delete(arr,np.where(arr==34))
 #flattening an array - row wise - C
 #flattening column wise - Fortran
 # 
-print(arr)
-flatened_arr = arr.flatten('F') #COLUMN WISE - COLUMN MAJOR
+# print(arr)
+arr2 = np.array([[ 1.07, 0.44 , 1.5  , 0.2  , 3.2 , 1.18 , 0.09 , 1.44  ,1.52 , 2.44,  0.78 , 0.02]
+ [ 0.27,  1.13 , 1.72,  4.14 , 2.66 , 0.61 , 1.03 , 1.4 , 18.16 , 2.24 , 0.29,  0.5 ]])
+flatened_arr = arr2.flatten('F') #COLUMN WISE - COLUMN MAJOR
 flatened_ar2 = arr.flatten('C')#ROW WISE - ROW MAJOR
 
-print(flatened_ar2)
+print(flatened_arr)
