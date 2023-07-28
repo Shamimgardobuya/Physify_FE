d6c4610f9cd8ecbd5df702394d7e6150011ad92d
Shamimgardobuya <shamimobuya@gmail.com>
2023-07-28 15:37:00 +0300

    more practise with backtracking and recursion

---
+++ backtrack.c
@@ -0,0 +1,19 @@
+//declare 2 arrays
+#include <stdio.h>
+int main()
+{
+    int myarr [2][4] = {
+        {2,3,4,8},
+        {3,4,5,8}
+};
+for(int i = 0;i<myarr.length;i++){
+    for (int j = 0;j< myarr.length;j++){
+printf("%d",myarr[i][j]);
+
+    }
+
+}
+// printf("%d",myarr[i][j]);
+return 0;
+};
+
---
+++ recursion_exercises.py
@@ -0,0 +1,38 @@
+#  Write a recursive function 
+# that accepts two numbers 
+# as its argument and returns its power
+#first argument, number then second number is for the power of the number
+#subtask,is - 
+#what if its 8,8, 8,7 86, 85, 84,83,82,81
+#n^n-1
+#if numb == 0
+#FIND BASE CASE #if numb == 0
+#create function power
+#inside parameters,
+#create param name n
+#creaet a base case
+#repeated subtask(n^n-1)
+#NUMB 
+#power decreasing
+#subtask is 8*7,
+#number get the power, repeating will be number^4,number
+#
+#Write a program that reads two integers from 
+#keyboard and calculate the greatest common 
+#divisor (gcd) using recursive function
+#base number - number
+#exponent the power - in python **
+def find_power(number):
+    if number == 0:
+        return number
+    else:
+        # print(find_power(number**number - 1))
+        return number ** find_power(number - 1)
+   
+# print(find_power(4))
+
+for i in range(0,4):
+    print(find_power(i))
+#0,1,2,3
+#
+
