20124605c2fbffcc735041d78b104747ec45b074
Shamimgardobuya <shamimobuya@gmail.com>
2023-07-31 18:26:22 +0300

    corrected bubble sort algorithm

--- bubblesort.py
+++ bubblesort.py
@@ -6,33 +6,36 @@ import random
 import re
 import sys
 
-def sort_with_bubble_sort(array):
+def sort_with_bubble_sort(my_array):
+    n = len(my_array)   
     number_swaps = 0
-    n = len(array) 
-    for i in range(n):
-        print(array)
-        for j in range(0,n-1):
-            if array[j] > array[j+1]:
-                array[j],array[j+1] = array[j+1], array[j]
-                number_swaps +=1
-        statement = f"Array is sorted in {number_swaps}swaps."
-        first = f"First Element : {array[0]}"
-        last = f"Last Element: {array[-1]}"
-        print(f"""{statement}
-            {first}
-            {last}
-            """)
+    #number of swaps is 0, i don't know why
+    # 🐵 yes, it is because of scopes that exist 
+    for i in range(n-1):
+        # number_swaps = 0
+        for j in range(0,n-i-1): #second iteration to start from where we left off true. inner loop only compares elements that need to be sorted
+            if my_array[j] > my_array[j+1]:
+                my_array[j],my_array[j+1] = my_array[j+1], my_array[j]
+                number_swaps +=1            
+        print(f"{number_swaps}\nfirst element is {my_array[0]}\nlast element is {my_array[-1]}")
         if(number_swaps==0):
            break
-        print("Array is sorted in 0 swaps.")
+        return f"Array is sorted in {number_swaps} swaps 😉"
     
     
 if __name__ == '__main__':
-    n = int(input().strip())
+    # n = int(input().strip())
 
-    a = list(map(int, input().rstrip().split()))
-    for i in range(n):
-        a.append(i)
-    sort_with_bubble_sort(a)
+    # a = list(map(int, input().rstrip().split()))
+    a,b,c = input().split()
+    new_arr = []
+    new_arr.append(int(a))
+    new_arr.append(int(b))
+    new_arr.append(int(c))
+
+    
+    # for i in range(new_arr):
+    #     a.append(i)
+    print(sort_with_bubble_sort(new_arr))
     # print(a)
     # Write your code here
