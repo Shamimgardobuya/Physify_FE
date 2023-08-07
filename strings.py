35578370f3f44675c9d6fd326a12589e886559ff
Shamimgardobuya <shamimobuya@gmail.com>
2023-08-07 20:29:40 +0300

    working with LInked lists, second time

---
+++ strings.py
@@ -0,0 +1,6 @@
+# How do you calculate the number of 
+# numerical digits in a string
+#can use ord()function which returns the unicode digit of the number/character
+#accepts a string as argument,  'string 0 to 9' have a unicode of 48 to 57. 
+#
+
